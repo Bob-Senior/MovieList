6dcc974d6714e5b0242e55b07a8b34232f6e64af
Bob-Senior <boburkosimov0@gmail.com>
2021-02-20 21:36:31 +0200

    MovieList

---
+++ MovieList/ModelManager/DataManager.swift
@@ -0,0 +1,48 @@
+//
+//  DataManager.swift
+//  MovieList
+//
+//  Created by BOBUR on 20/02/2021.
+//
+
+import Foundation
+
+ class DataManager {
+    static let shared = DataManager()
+    
+    var movie = [
+    "Apry Men",
+        "Dark Night",
+        "Fight Club",
+        "Forest Gump",
+        "Good Bad Ugly",
+        "Inception",
+        "Starwars",
+        "The Godfather",
+        ]
+
+        var genre = [
+        "Crime",
+        "Action",
+        "Drama",
+        "Drama",
+        "Western",
+        "Sci-Fi",
+        "Fantasy",
+        "Crime",
+        ]
+
+        var fullDetail = [
+        "Apry Men - Crime",
+        "Dark Night - Action",
+        "Fight Club - Drama",
+        "Forest Gump - Drama",
+        "Good Bad Ugly - Western",
+        "Inception - Sci-Fi",
+        "Starwars - Fantasy",
+        "The Godfather - Crime",
+        
+        ]
+        }
+    
+ 
